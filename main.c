e9749de3dc05551e02df13b0ad628f2474208e07
DaviMaiaF <121465789+DaviMaiaF@users.noreply.github.com>
2023-08-17 17:18:25 +0000

    Criei dois carros utilizando caracteres gráficos ilustrado no livro de programação C

--- main.c
+++ main.c
@@ -6,4 +6,14 @@ int main(void) {
   printf("%-3.1f\n",3456.78);
   printf("%10.3f\n",3456.78);
   printf("%c  \n", 3393);
+
+  printf("\n\n\n");
+  printf("\n \xDC\xDC\xDB\xDB\xDB\xDB\xDC\xDC");
+  printf("\n \xDFO\xDf\xDf\xDf\xDFO\xDF");
+  printf("\n\n\n");
+  printf("\n \xDC\xDC\xDB \xDB\xDB\xDB\xDB\xDB\xDB");
+  printf("\n \xDFO\xDf\xDf\xDF\xDF\xDFOO\xDF");
+  printf("\n\n\n");
+
+  
 }
