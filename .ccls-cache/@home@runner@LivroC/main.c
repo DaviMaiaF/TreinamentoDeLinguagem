8d5aee667b2cb4317306ae3a616852c12390ee56
DaviMaiaF <121465789+DaviMaiaF@users.noreply.github.com>
2023-08-17 17:03:54 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@LivroC/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+  printf("%-4.2f\n",3456.78);
+  printf("%-3.2f\n",3456.78);
+  printf("%-3.1f\n",3456.78);
+  printf("%10.3f\n",3456.78);
+  printf("%c  \n", 3393);
+}
