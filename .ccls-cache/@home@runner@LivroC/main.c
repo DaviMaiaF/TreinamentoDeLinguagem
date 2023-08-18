1c3b2eccb5e74077312fb255154cf48e9bd1b421
DaviMaiaF <121465789+DaviMaiaF@users.noreply.github.com>
2023-08-18 17:50:59 +0000

    Foi criada a opção em que se converte Fahrenheit para Celsius.

--- .ccls-cache/@home@runner@LivroC/main.c
+++ .ccls-cache/@home@runner@LivroC/main.c
@@ -7,6 +7,7 @@ int main(void) {
   printf("%10.3f\n",3456.78);
   printf("%c  \n", 3393);
 
+  //Desenho de dois carros
   printf("\n\n\n");
   printf("\n \xDC\xDC\xDB\xDB\xDB\xDB\xDC\xDC");
   printf("\n \xDFO\xDf\xDf\xDf\xDFO\xDF");
@@ -15,5 +16,15 @@ int main(void) {
   printf("\n \xDFO\xDf\xDf\xDF\xDF\xDFOO\xDF");
   printf("\n\n\n");
 
-  
+  //Desenho de uma caixa
+  printf("\xC9\xCD\xBB\n");
+  printf("\xBA\xBA\n");
+  printf("\xC8\xCD\xBC\n");
+
+  //Mudança de Fahrenheit para Celsius 
+  int ftemp, ctemp;
+  printf("Digite a temperatura em graus Fahrenheit: ");
+  scanf("%d", &ftemp);
+  ctemp=(ftemp-32) * 5/9;
+  printf("Temperatura em graus Celsius e' %d", ctemp);
 }
